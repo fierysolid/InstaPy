1e13ca65236ce01982028863d0c5fea0ce6b01f5
Jordan Schoen <schoen.jordan@gmail.com>
2018-08-22 20:20:37 -0400

    adding schedule

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -1,34 +1,25 @@
-import random 
 from instapy import InstaPy
-import os
-import multiprocessing
-import datetime
+import schedule
 import time
 
-instaUser = ['schoennyc']#, 'stal_style']
-instaPass = ['theman']#, 'Dylan9790']
-smartTags = [['a7sii', 'a7s', 'rokinon', 'sonyalpha', 'atomos', 'film', '35mm']]#, ['styledbystal', 'style', 'stylist', 'ootd', 'fashion', 'nycstylist', 'nycstyle', 'lookbook', 'fashiongram', 'wiwt', 'lookoftheday', 'stylish', 'streetstyle', 'instastyle', 'styleinspiration', 'empower', 'women', 'instafashion', 'fashionblogger', 'fashionista', 'streetstyle', 'stylish', 'mensfashion', 'instastyle', 'lookbook', 'whatiwore', 'styleinspo', 'styleblogger']]
+def job():
+    try:
+        insta_username = 'schoennyc'
+        insta_password = 'theman'
+        smartTags = ['a7sii', 'a7s', 'rokinon', 'sonyalpha', 'atomos', 'film', '35mm']
+        bot = InstaPy(username=insta_username, password=insta_password, selenium_local_session=False)
+        bot.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
+        bot.login()
+        bot.set_smart_hashtags(smartTags, limit=3, sort='top', log_tags=True)
+        bot.like_by_tags(amount=random.randint(2,5), use_smart_hashtags=True)
+        bot.end()
+    except:
+        import traceback
+        print(traceback.format_exc())
+        
+schedule.every().day.at("9:00").do(job)
+schedule.every().day.at("21:00").do(job)
 
-def worker(selection):
-    
-    print("MULTI - Started as",instaUser[selection],"at",datetime.datetime.now().strftime("%H:%M:%S"))
-    session = InstaPy(username=instaUser[selection], password=instaPass[selection], selenium_local_session=False)
-    session.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
-    session.login()
-   
-    if smartTags[selection]:
-        session.set_smart_hashtags(smartTags[selection], limit=3, sort='top', log_tags=True)
-        session.like_by_tags(amount=random.randint(2,5), use_smart_hashtags=True)
-        print("MULTI -",instaUser[selection],"finished smartTags at",datetime.datetime.now().strftime("%H:%M:%S"))
-
-    session.end()
-    print("MULTI -",instaUser[selection],"finished run at",datetime.datetime.now().strftime("%H:%M:%S"))
-
-if __name__ == '__main__':        
-    print("MULTI -","Starting at",datetime.datetime.now().strftime("%H:%M:%S"))
-    jobs = []
-    for i in range(len(instaUser)):
-        p = multiprocessing.Process(target=worker, args=(i,))
-        jobs.append(p)
-        p.start()
-        time.sleep(66);#no delay cause some instances of chrome to give errors and stop
+while True:
+    schedule.run_pending()
+    time.sleep(1)
