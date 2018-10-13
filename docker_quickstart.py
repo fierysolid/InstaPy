4ad9558fffac3c1f8a8d79758c39b76535867f15
Jordan Schoen <schoen.jordan@gmail.com>
2018-10-13 12:40:14 -0400

    adding crystals info

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -1,6 +1,6 @@
 from instapy import InstaPy
-insta_username = 'schoennyc'
-insta_password = 'theman'
+insta_username = 'stal_style'
+insta_password = 'Dylan9790'
 
 bot = InstaPy(username=insta_username, password=insta_password, selenium_local_session=False)
 bot.set_selenium_remote_session(selenium_url='http://selenium:4444/wd/hub')
@@ -10,39 +10,22 @@ bot.set_relationship_bounds(enabled=True,
   delimit_by_numbers=True,
   min_followers=40,
   min_following=40)
-bot.like_by_tags(['a7sii',
-    'a7s',
-    'rokinon',
-    'sonyalpha',
-    'atomos',
-    'film',
-    '35mm',
-    'film',
-    'filmphotographyfilmisnotdead',
-    '35mm',
-    'analog',
-    'movie',
-    'ishootfilm',
-    'cinema',
-    'filmcamera',
-    'believeinfilm',
-    'filmcommunity',
-    'analogue',
-    'kodak',
-    'analogphotography',
-    'staybrokeshootfilm',
-    'filmfeed',
-    'movies',
-    'buyfilmnotmegapixels',
-    'thefilmcommunity',
-    'shootfilm',
-    'socialdraft',
-    '35mmfilm',
-    'filmmaking',
-    'actor ',
-    'keepfilmalive',
-    'filmphoto',
-    'films',
-    'filmmaker',
-    'mediumformat'], amount=100)
+bot.like_by_tags([
+    pink,
+    underwear,
+    instadaily,
+    picoftheday,
+    instapic,
+    instagood,
+    glamour,
+    style,
+    mood,
+    newyorkcity,
+    fashion,
+    love,
+    beautiful,
+    happy,
+    women,
+    empower
+], amount=100)
 bot.end()
